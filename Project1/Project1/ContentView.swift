0c162d85ca227ace43bdf20d55d09763026180a2
brittneyvierra <86032192+brittneyvierra@users.noreply.github.com>
2021-09-15 17:06:00 -0700

    Add files via upload

---
+++ Project1/Project1/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by brittney vierra on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("Tap on the dog to see description")
+                .padding()
+        }
+        VStack{
+            HStack{
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by brittney vierra on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
